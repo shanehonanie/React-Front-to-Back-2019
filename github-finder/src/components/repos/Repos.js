d082d888f2794ca9032323217a46ee42f40d4464
Shane Honanie <shaneh425@gmail.com>
2019-08-29 16:38:28 -0700

    User to use useEffect Hook

--- github-finder/src/components/repos/Repos.js
+++ github-finder/src/components/repos/Repos.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import RepoItem from './RepoItem';
 
 const Repos = ({ repos }) => {
 	return repos.map(repo => <RepoItem repo={repo} key={repo.id} />);
 };
 
-Repos.propTypes = {
-	repo: PropTypes.array.isRequired
-};
+// Repos.propTypes = {
+// 	repo: PropTypes.array.isRequired
+// };
 
 export default Repos;
