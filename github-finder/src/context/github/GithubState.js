11d2eb0e54ba75812980d91978e8a8a178ec5d2c
Shane Honanie <shaneh425@gmail.com>
2019-08-30 12:52:37 -0700

    Move Repos State to Context

--- github-finder/src/context/github/GithubState.js
+++ github-finder/src/context/github/GithubState.js
@@ -2,7 +2,13 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import GithubContext from './GithubContext';
 import GithubReducer from './GithubReducer';
-import { SEARCH_USERS, SET_LOADING, CLEAR_USERS, GET_USER } from '../types';
+import {
+	SEARCH_USERS,
+	SET_LOADING,
+	CLEAR_USERS,
+	GET_USER,
+	GET_REPOS
+} from '../types';
 
 const GithubState = props => {
 	const initialState = {
@@ -43,6 +49,18 @@ const GithubState = props => {
 	};
 
 	// Get Repos
+	const getUserRepos = async username => {
+		setLoading();
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		dispatch({
+			type: GET_REPOS,
+			payload: res.data
+		});
+	};
 
 	// Clear Users
 	const clearUsers = () => dispatch({ type: CLEAR_USERS });
@@ -59,7 +77,8 @@ const GithubState = props => {
 				loading: state.loading,
 				searchUsers,
 				clearUsers,
-				getUser
+				getUser,
+				getUserRepos
 			}}
 		>
 			{props.children}
