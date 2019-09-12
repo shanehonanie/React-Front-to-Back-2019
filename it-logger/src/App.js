cd00c11ce852f9afdd3d2f1aa389d30301aeb22b
Shane Honanie <shaneh425@gmail.com>
2019-09-12 15:18:35 -0700

    Materialize Setup

--- it-logger/src/App.js
+++ it-logger/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min';
 import './App.css';
 
 const App = () => {
+	useEffect(() => {
+		// Init MaterializeJS
+		M.AutoInit();
+	});
 	return <div className='App'>My App</div>;
 };
 
